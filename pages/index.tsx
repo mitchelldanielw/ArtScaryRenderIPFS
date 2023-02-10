e17b4a5819684de99142e8002bf4b514522a98f1
Mitchell45432 <Mitchell45432@gmail.com>
2023-02-10 15:10:21 -0500

    Configure and yarn

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,51 +1,145 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { MediaRenderer, ConnectWallet } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
+      <h2 className={styles.title}>
+        <a href="https://ipfs.io/ipfs/QmYQtdtaGEcoXP67tyTk7PcyBgLTRaQsUzx7j69rn2AE92?filename=S0.webp">ArtScary by mitchelldanielw</a>
+      </h2>
+      <div className={styles.connect}>
+        <ConnectWallet />
+      </div>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
-        </h1>
-
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
-        </p>
-
-        <div className={styles.connect}>
-          <ConnectWallet />
-        </div>
+        
 
         <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
+          <a href="https://ipfs.io/ipfs/QmYQtdtaGEcoXP67tyTk7PcyBgLTRaQsUzx7j69rn2AE92?filename=S0.webp"
+            className={styles.card}>
+            <MediaRenderer
+              src="ipfs://QmYQtdtaGEcoXP67tyTk7PcyBgLTRaQsUzx7j69rn2AE92"
+              alt="ArtScary0"
+            /></a>
 
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
+          <a href="https://ipfs.io/ipfs/QmRxvCm4QZ9dDEEokoCH3qoHz5Dh2GkeNB7rccGYq4ffe4?filename=S1.webp"
+            className={styles.card}>
+            <MediaRenderer
+              src="ipfs://QmRxvCm4QZ9dDEEokoCH3qoHz5Dh2GkeNB7rccGYq4ffe4"
+              alt="ArtScary0"
+            /></a>
 
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
+          <a href="https://ipfs.io/ipfs/QmUYjGjP9Los7uQvVvuUx1RjhCsaQfQX57QRgEMiGa8B4z?filename=S2.webp"
+            className={styles.card}>
+            <MediaRenderer
+              src="ipfs://QmUYjGjP9Los7uQvVvuUx1RjhCsaQfQX57QRgEMiGa8B4z"
+              alt="ArtScary0"
+            /></a>
+
+          <a href="https://ipfs.io/ipfs/QmcekVqgAGAvjqk6ozCxq7aFgnPFpUg8zMCEovu8MRdzkd?filename=S3.webp"
+            className={styles.card}>
+            <MediaRenderer
+              src="ipfs://QmcekVqgAGAvjqk6ozCxq7aFgnPFpUg8zMCEovu8MRdzkd"
+              alt="ArtScary0"
+            /></a>
+
+          <a href="https://ipfs.io/ipfs/QmTVhx76EGPVcW7EDdnaNDJWACSERxjMU6yJQz9eBH8Uks?filename=S4.webp"
+            className={styles.card}>
+            <MediaRenderer
+              src="ipfs://QmTVhx76EGPVcW7EDdnaNDJWACSERxjMU6yJQz9eBH8Uks"
+              alt="ArtScary0"
+            /></a>
+
+          <a href="https://ipfs.io/ipfs/QmeKG37juFjNxBxNznJvphHWARacm6WPv63FMeou4EEihS?filename=S5.webp"
+            className={styles.card}>
+            <MediaRenderer
+              src="ipfs://QmeKG37juFjNxBxNznJvphHWARacm6WPv63FMeou4EEihS"
+              alt="ArtScary0"
+            /></a>
+
+          <a href="https://ipfs.io/ipfs/QmX72VbkP3YLWv6qfoucT288PGNcrDiMGcgPM7zqtuvBfe?filename=S6.webp"
+            className={styles.card}>
+            <MediaRenderer
+              src="ipfs://QmX72VbkP3YLWv6qfoucT288PGNcrDiMGcgPM7zqtuvBfe"
+              alt="ArtScary0"
+            /></a>
+            
+            <a href="https://ipfs.io/ipfs/QmWXCqViEKiJTPDnanh4BaAT4equkRQCL4BQYrGsJHsMYY?filename=S7.webp"
+            className={styles.card}>
+            <MediaRenderer
+              src="ipfs://QmWXCqViEKiJTPDnanh4BaAT4equkRQCL4BQYrGsJHsMYY"
+              alt="ArtScary0"
+            /></a>
+
+          <a href="https://ipfs.io/ipfs/QmYPSZJc1QzRo5rGbhcw6qre2EU7raDuUQKAHkHLmUzhg7?filename=S8.webp"
+            className={styles.card}>
+            <MediaRenderer
+              src="ipfs://QmYPSZJc1QzRo5rGbhcw6qre2EU7raDuUQKAHkHLmUzhg7"
+              alt="ArtScary0"
+            /></a>
+
+          <a href="https://ipfs.io/ipfs/QmbAE336mtfN9CjWRsZxgV4hwmeTNjawPFXGQoN2c2CHQ1?filename=S9.webp"
+            className={styles.card}>
+            <MediaRenderer
+              src="ipfs://QmbAE336mtfN9CjWRsZxgV4hwmeTNjawPFXGQoN2c2CHQ1"
+              alt="ArtScary0"
+            /></a>
+
+          <a href="https://ipfs.io/ipfs/QmQQm4XpEUJ4Uf5GU7YoyfftZidLMJL6MAzMHDJe41kaX9?filename=S10.webp"
+            className={styles.card}>
+            <MediaRenderer
+              src="ipfs://QmQQm4XpEUJ4Uf5GU7YoyfftZidLMJL6MAzMHDJe41kaX9"
+              alt="ArtScary0"
+            /></a>
+
+          <a href="https://ipfs.io/ipfs/QmfCtybviCwXkHLmabJiaE5cbRqUNc66ybsnkDNNZ2Fpyk?filename=S11.webp"
+            className={styles.card}>
+            <MediaRenderer
+              src="ipfs://QmfCtybviCwXkHLmabJiaE5cbRqUNc66ybsnkDNNZ2Fpyk"
+              alt="ArtScary0"
+            /></a>
+
+          <a href="https://ipfs.io/ipfs/QmRxZRiDvDC6snMVwSFzTcUHUwZzwrHYvKTFJJYAXZTnTw?filename=S12.webp"
+            className={styles.card}>
+            <MediaRenderer
+              src="ipfs://QmRxZRiDvDC6snMVwSFzTcUHUwZzwrHYvKTFJJYAXZTnTw"
+              alt="ArtScary0"
+            /></a>
+
+          <a href="https://ipfs.io/ipfs/QmZ9BzY3fugns6mqqWapRCL6tMv7kT5ViRcFfJFSQBDVR7?filename=S13.webp"
+            className={styles.card}>
+            <MediaRenderer
+              src="ipfs://QmZ9BzY3fugns6mqqWapRCL6tMv7kT5ViRcFfJFSQBDVR7"
+              alt="ArtScary0"
+            /></a>
+
+          <a href="https://ipfs.io/ipfs/QmbuaaL7vBo54ikAz9PqNRNPQawt4zpP8t8df2EJRYUQNc?filename=S14.webp"
+            className={styles.card}>
+            <MediaRenderer
+              src="ipfs://QmbuaaL7vBo54ikAz9PqNRNPQawt4zpP8t8df2EJRYUQNc"
+              alt="ArtScary0"
+            /></a>
+
+          <a href="https://ipfs.io/ipfs/QmUbgs8rVmVzt7tzKgaC3yk7yQVQgPT11XC7Hs4xZzxAXc?filename=S15.webp"
+            className={styles.card}>
+            <MediaRenderer
+              src="ipfs://QmUbgs8rVmVzt7tzKgaC3yk7yQVQgPT11XC7Hs4xZzxAXc"
+              alt="ArtScary0"
+            /></a>
+
+          <a href="https://ipfs.io/ipfs/QmfKQibbcKWiST9pYV4wKM247pR4z1JaUkzCH4PuEeH8KB?filename=S16.webp"
+            className={styles.card}>
+            <MediaRenderer
+              src="ipfs://QmfKQibbcKWiST9pYV4wKM247pR4z1JaUkzCH4PuEeH8KB"
+              alt="ArtScary0"
+            /></a>
+
+          <a href="https://ipfs.io/ipfs/QmYNQ6TvJ7GiATKHVMAD79CFvqPXpQ9dJKVU9aBvHre7mr?filename=S17.webp"
+            className={styles.card}>
+            <MediaRenderer
+              src="ipfs://QmYNQ6TvJ7GiATKHVMAD79CFvqPXpQ9dJKVU9aBvHre7mr"
+              alt="ArtScary0"
+            /></a>
         </div>
       </main>
     </div>
